d885a0b2c7aa0081f2add53cee198cbc5dc3cde9
Diaa-Essam <studywithdod@gmail.com>
2026-05-03 07:58:52 +0200

    Add Matched filter implementation and AWGN loop

--- part1_matched_filter/src/main_part1.m
+++ part1_matched_filter/src/main_part1.m
@@ -13,9 +13,13 @@ snr_db = 0:0.2:30;
 
 BER = zeros(size(snr_db));
 
+s1 = ones(1, m);
+s2 = zeros(1, m);
+
+h_mf = fliplr(s1 - s2);
+
 for k = 1:length(snr_db)
     
     rx_signal = awgn(tx_signal, snr_db(k), 'measured');
-    
-    
+    mf_output = conv(rx_signal, h_mf);
 end
