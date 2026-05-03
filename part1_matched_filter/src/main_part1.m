89e6f89e82f2e697f9be36c1e7dc81550a11f652
Diaa-Essam <studywithdod@gmail.com>
2026-05-03 07:40:58 +0200

    Add AWGN channel with SNR loop for BER simulation

--- part1_matched_filter/src/main_part1.m
+++ part1_matched_filter/src/main_part1.m
@@ -8,3 +8,14 @@ bits = randi([0 1], 1, N);
 
 m = 20;
 tx_signal = repelem(bits, m);
+
+snr_db = 0:0.2:30;
+
+BER = zeros(size(snr_db));
+
+for k = 1:length(snr_db)
+    
+    rx_signal = awgn(tx_signal, snr_db(k), 'measured');
+    
+    
+end
