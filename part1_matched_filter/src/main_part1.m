67c162fe568c4f29ec965dcbcc9c2634286f205f
Diaa-Essam <studywithdod@gmail.com>
2026-05-07 10:18:50 +0200

    Implement correlator receiver with BER calculation

--- part1_matched_filter/src/main_part1.m
+++ part1_matched_filter/src/main_part1.m
@@ -15,21 +15,20 @@ BER = zeros(size(snr_db));
 
 s1 = ones(1, m);
 s2 = zeros(1, m);
+g = s1 - s2;
+g_rep = repmat(g, 1, N);
 
-h_mf = fliplr(s1 - s2);
+threshold = sum(g .* s1) / 2;
 
 for k = 1:length(snr_db)
     
     rx_signal = awgn(tx_signal, snr_db(k), 'measured');
+    corr_product = rx_signal .* g_rep;
     
-    mf_output = conv(rx_signal, h_mf);
+    corr_matrix = reshape(corr_product, m, N);
+    corr_output = sum(corr_matrix, 1);
     
-    sampled_output = mf_output(m:m:end);
-    
-    threshold  = m / 2;
-    
-    
-    detected_bits = sampled_output > threshold;
+    detected_bits = corr_output > threshold; 
     BER(k) = sum(xor(bits, detected_bits)) / N;
 
 end
